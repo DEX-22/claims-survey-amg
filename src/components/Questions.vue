8c2f8595d35726697f911e8e7cad4dc86a38723d
chriscass <christiancasafrancarocha@gmail.com>
2023-06-17 01:03:00 -0500

    header and questions test

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,13 +1,183 @@
-<template lang="">
-    <div>
-        <!-- <h2>Component Questions</h2> -->
+<template>
+    <div id="app" :class="{ submitted: is_submitted }">
+      <form>
+        <h1>Give Your Feedback</h1>
+        <rating-feedback :details="questions[curIndex]" :total-rate="totalRate"></rating-feedback>
+        <div class="button_wrapper">
+          <div @click="changeQuestion(-1)" :class="['button', { disabled: curIndex == 0 }]">Prev</div>
+          <div @click="changeQuestion(1)" :class="['button', { disabled: curIndex == questions.length - 1 }]">Next</div>
+        </div>
+        <div class="submit_button_wrapper">
+          <div class="button" @click="submit()">Submit</div>
+        </div>
+      </form>
+      <div class="thank_you_wrapper">
+        <h2>Thank You for Submitting Feedback</h2>
+        <h3>Refresh the page to see/change your feedback</h3>
+      </div>
     </div>
 </template>
-<script>
-export default {
-    
-}
-</script>
-<style lang="">
-    
-</style>
+  
+  <script>
+  import RatingFeedback from './RatingFeedback.vue';
+  
+  export default {
+    components: {
+      RatingFeedback,
+    },
+    data() {
+      return {
+        totalRate: 5,
+        questions: [
+          {
+            question: 'How was our product?',
+            rate: null
+          },
+          {
+            question: 'How was our service?',
+            rate: null
+          },
+          {
+            question: 'How was our staff?',
+            rate: null
+          },
+          {
+            question: 'How was our website?',
+            rate: null
+          },
+          {
+            question: 'How was your experience?',
+            rate: null
+          }
+        ],
+        curIndex: 0,
+        is_submitted: false
+      };
+    },
+    methods: {
+      changeQuestion(index) {
+        var new_index = this.curIndex + index;
+  
+        if (new_index < 0 || new_index >= this.questions.length)
+          return;
+  
+        this.curIndex = new_index;
+      },
+      submit() {
+        localStorage.setItem('questionDetails', JSON.stringify(this.questions));
+        this.is_submitted = true;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  
+  #app {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px 0px;
+    text-align: center;
+    color: #123D61;
+  }
+  
+  #app > .thank_you_wrapper {
+    display: none;
+  }
+  
+  #app.submitted > form {
+    display: none;
+  }
+  
+  #app.submitted > .thank_you_wrapper {
+    display: block;
+  }
+  
+  h1 {
+    font-size: 32px;
+    padding-bottom: 30px;
+    border-bottom: 1px solid #AAA;
+    margin-bottom: 30px;
+    text-shadow: 0px 5px 10px #888;
+  }
+  
+  .button_wrapper {
+    margin: 20px 10px;
+  }
+  
+  .button_wrapper > .button,
+  .submit_button_wrapper > .button {
+    width: 100px;
+    padding: 7px;
+    border: 1px solid #123D61;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+  
+  .button_wrapper > .button:first-child {
+    float: left;
+  }
+  
+  .button_wrapper > .button:last-child {
+    float: right;
+  }
+  
+  .button_wrapper:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+  
+  .button_wrapper > .button:hover {
+    box-shadow: 0px 0px 20px 1px #123D61;
+  }
+  
+  .button_wrapper > .button.disabled {
+    cursor: auto;
+    opacity: 0.3;
+    box-shadow: none !important;
+  }
+  
+  .submit_button_wrapper {
+    padding-top: 30px;
+    border-top: 1px solid #AAA;
+    margin-top: 30px;
+  }
+  
+  .submit_button_wrapper > .button,
+  .main_link {
+    width: 120px;
+    padding: 10px;
+    margin: 0 auto;
+    background: #123D61;
+    color: #FFF;
+    border-radius: 7px;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+  
+  .submit_button_wrapper > .button:hover,
+  .main_link:hover {
+    box-shadow: 0px 0px 20px 1px #123D61;
+  }
+  
+  .thank_you_wrapper > h2 {
+    font-size: 40px;
+    padding-bottom: 20px;
+  }
+  
+  .thank_you_wrapper > h3 {
+    font-size: 24px;
+  }
+  
+  .main_link {
+    display: block;
+    position: fixed;
+    right: 10px;
+    top: 10px;
+    text-align: center;
+  }
+  
+  
+  </style>
---
+++ src/components/RatingFeedback.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="question_wrapper">
+      <h3 v-html="details.question"></h3>
+      <ul>
+        <li v-for="i in totalRate">
+          <label>
+            <input type="radio" v-model="details.rate" :value="i" name="question-rate">
+            <span>{{ i }}</span>
+          </label>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['details', 'totalRate']
+  };
+  </script>
+  
+  <style scoped>
+  .question_wrapper > h3 {
+    font-size: 21px;
+    padding-bottom: 20px;
+  }
+  
+  .question_wrapper > ul > li {
+    display: inline-block;
+    margin-right: 30px;
+  }
+  
+  .question_wrapper > ul > li:last-child {
+    margin-right: 0px;
+  }
+  
+  .question_wrapper > ul > li > label {
+    cursor: pointer;
+  }
+  
+  .question_wrapper > ul > li > label > input {
+    display: none;
+  }
+  
+  .question_wrapper > ul > li > label > span {
+    display: block;
+    position: relative;
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+    border-radius: 50%;
+    border: 1px solid #123D61;
+    font-size: 20px;
+    transition: all 0.3s;
+  }
+  
+  .question_wrapper > ul > li > label:hover > span {
+    box-shadow: 0px 0px 20px 1px #123D61;
+  }
+  
+  .question_wrapper > ul > li > label > input:checked ~ span {
+    color: #FFF;
+    border: 1px solid transparent;
+  }
+  
+  .question_wrapper > ul > li > label > span:before {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 0%;
+    height: 0%;
+    background: #123D61;
+    border-radius: 50%;
+    opacity: 0;
+    z-index: -1;
+    transition: all 0.3s;
+  }
+  
+  .question_wrapper > ul > li > label > input:checked ~ span:before {
+    left: 0%;
+    top: 0%;
+    width: 100%;
+    height: 100%;
+    opacity: 1;
+  }
+  </style>
+  
