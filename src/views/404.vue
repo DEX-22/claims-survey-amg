264182556839cd6f9ab24ff0bdd18bd9ca8c7a49
Enrique Saenz <enrique.saenz@alarcongroup.us>
2023-06-21 15:45:12 -0500

    add 404 view

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,5 +1,16 @@
 <template>
-    <h2>
-        404 | NOT FOUND
-    </h2>
+    <div class="not-found"> 
+        <!-- <img :src="'./assets/images/404-page-not-found.png'" alt="not found"> -->
+    </div>
 </template>
+<style scoped>
+ .not-found{
+    background-image: url('../assets/images/404-page-not-found.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    width: 100vw;
+    height: 100vh;
+    
+ }
+</style>
